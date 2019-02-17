e86caa77f2a5163debfe9c2bc9451dfc6af536f4
calebturcotte <calebturcotte@cmail.carleton.ca>
2019-02-16 19:41:45 -0500

    commit

--- app/src/main/java/com/example/tastebud/MainActivity.java
+++ app/src/main/java/com/example/tastebud/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.tastebud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-
+import android.view.View;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
-
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
     String [] SPINNERLIST={"INDIA", "AUSTRALIA", "SOUTH AFRICA", "NEW ZEALAND"};
+    Integer count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         MaterialBetterSpinner betterSpinner=(MaterialBetterSpinner)findViewById(R.id.android_material_design_spinner);
         betterSpinner.setAdapter(arrayAdapter);
 
+    }
+
+    public void buttonOnClick(View v) {
+            Button button = (Button) v;
+        ((Button) v).setText(count.toString());
+        count++;
     }
 
 
