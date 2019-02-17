7afcc3e7c55f802c3a5482154297d6ab93bde06f
calebturcotte <calebturcotte@cmail.carleton.ca>
2019-02-16 19:18:54 -0500

    layout

--- app/src/main/java/com/example/tastebud/MainActivity.java
+++ app/src/main/java/com/example/tastebud/MainActivity.java
@@ -2,21 +2,24 @@ package com.example.tastebud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    String [] SPINNERLIST={"INDIA", "AUSTRALIA", "SOUTH AFRICA", "NEW ZEALAND"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
+        MaterialBetterSpinner betterSpinner=(MaterialBetterSpinner)findViewById(R.id.android_material_design_spinner);
+        betterSpinner.setAdapter(arrayAdapter);
+
     }
 
-    /*Button button = (Button) rootView.findViewById(R.id.my_button);
-button.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            doSomething();
-        }
-    });*/
+
 }
