26e735424461f1396a6bb323b65723898c5653f6
calebturcotte <calebturcotte@cmail.carleton.ca>
2019-02-16 21:36:37 -0500

    commm

--- app/src/main/java/com/example/tastebud/Map.java
+++ app/src/main/java/com/example/tastebud/Map.java
@@ -18,17 +18,7 @@ public class Map extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-/*
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
 
-        });
-*/
     Intent intent = getIntent();
     String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
